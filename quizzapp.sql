0fb1aeadebf71e227dcf46f5677eca408e7e853b
Carlos Ivan Aguero <aguero_5209@hotmail.com>
2020-12-03 23:07:26 -0600

    identacion

--- quizzapp.sql
+++ quizzapp.sql
@@ -1,13 +1,10 @@
 ﻿-- Diseñar la estructura de la base de datos 
 -- Diseñar los escenarios
 -- Ejecutamos los querys de los cuales extraeremos la base de datos
-
 --tabla de  preguntas
 -- tabla de temas 
 -- tabla de configuración 
 -- tabla de juego 
-
-
 -- TEMAS DE PREGUNTAS
 -- 
 DROP TABLE IF EXISTS [configuration_themes];
@@ -16,45 +13,39 @@ DROP TABLE IF EXISTS [question_answers];
 DROP TABLE IF EXISTS [questions];
 DROP TABLE IF EXISTS [themes];
 DROP TABLE IF EXISTS [configurations];
-
 -- Tabla de usuarios
 CREATE TABLE [usuario](
   [id_usuario] INTEGER PRIMARY KEY AUTOINCREMENT,
   [nombre_usuario] INTEGER NOT NULL,
-  [score_usuario] INTEGER NOT NULL,  
+  [score_usuario] INTEGER NOT NULL,
   [id_configuration] INTEGER REFERENCES [configurations] (id_configuration)
 );
-
 -- Tabla de configuraciones
 CREATE TABLE [configurations](
   [id_configuration] INTEGER PRIMARY KEY AUTOINCREMENT,
-  [number_questions] INTEGER NOT NULL, 
+  [number_questions] INTEGER NOT NULL,
   [dificulty] TEXT NOT NULL,
   [number_tracks] INTEGER NOT NULL,
-  [tracks_enabled] BOOLEAN NOT NULL  
+  [tracks_enabled] BOOLEAN NOT NULL
 );
-
 CREATE TABLE [configuration_themes](
-  [id_configuration] INTEGER REFERENCES [configurations](id_configuration), 
-  [id_theme] INTEGER REFERENCES [themes](id));
-
+  [id_configuration] INTEGER REFERENCES [configurations](id_configuration),
+  [id_theme] INTEGER REFERENCES [themes](id)
+);
 -- Tabla de temas
 CREATE TABLE [themes](
-  [id] INTEGER PRIMARY KEY AUTOINCREMENT, 
+  [id] INTEGER PRIMARY KEY AUTOINCREMENT,
   [description] TEXT UNIQUE NOT NULL
 );
-
 -- Tabla de preguntas de temas
 CREATE TABLE [questions](
-   [id] INTEGER PRIMARY KEY AUTOINCREMENT, 
-   [theme_id] INTEGER REFERENCES [themes]([id]),
-   [text] TEXT UNIQUE NOT NULL   
+  [id] INTEGER PRIMARY KEY AUTOINCREMENT,
+  [theme_id] INTEGER REFERENCES [themes]([id]),
+  [text] TEXT UNIQUE NOT NULL
 );
-
 -- Tabla de respuestas de preguntas de temas
 CREATE TABLE [question_answers](
-   [id] INTEGER REFERENCES [questions] ([id]), 
-   [text] TEXT NOT NULL, 
-   [correct_answer] INTEGER NOT NULL
+  [id] INTEGER REFERENCES [questions] ([id]),
+  [text] TEXT NOT NULL,
+  [correct_answer] INTEGER NOT NULL
 );
-
